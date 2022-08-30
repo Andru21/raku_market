3d18fa19c985e810ebdcd0431ca6e7d4bf992c32
Raku <juan.marulanda@optima.net.co>
2022-08-30 18:03:48 -0500

    SecondCommit

--- src/main/java/com/raku/market/web/controller/HelloWorldController.java
+++ src/main/java/com/raku/market/web/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.raku.market;
+package com.raku.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
